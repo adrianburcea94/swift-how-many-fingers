3b59620c84a3c8953611c4eb1120eb1064073e93
Burcea Adrian <adrianburcea@192-168-0-112.rdsnet.ro>
2018-03-02 22:05:21 +0200

    Initial Commit

---
+++ How Many Fingers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  How Many Fingers
+//
+//  Created by Adrian Burcea on 02/03/2018.
+//  Copyright © 2018 DOOZY SOFTWARE SOLUTIONS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
