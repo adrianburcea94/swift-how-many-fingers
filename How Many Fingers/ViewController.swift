72af40b8e78830e27cf2a5ef908298cfd8e65278
Adrian Burcea <adrianburcea@192-168-0-112.rdsnet.ro>
2018-03-02 22:37:13 +0200

    Initial commit

--- How Many Fingers/ViewController.swift
+++ How Many Fingers/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberField: UITextField!
+    @IBOutlet weak var resultField: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func guessNumber(_ sender: UIButton) {
+        let diceRoll: Int = Int(arc4random_uniform(6))
+        if let numberString = numberField.text {
+            if let yourNumber = (Int)(numberString) {
+                if yourNumber == diceRoll {
+                    resultField.textColor = UIColor.green
+                    resultField.text = "You've guessed"
+                } else {
+                    resultField.textColor = UIColor.red
+                    resultField.text = "You're wrong. It was \(diceRoll)"
+                }
+            }
+        }
+    }
+    // let diceRoll - arc4random_uniform(6) - generates a random number between 0 and 5
 }
 
